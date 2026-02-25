2c864896947645ea604eb6edd36863eb893e1b99
hollo112 <76999794+hollo112@users.noreply.github.com>
2026-02-25 14:57:46 +0900

    feat: PhotonRoomManager구현

---
+++ Assets/02.Scripts/Core/PhotonRoomManager.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class PhotonRoomManager : MonoBehaviourPunCallbacks
+{
+    public static PhotonRoomManager Instance{get; private set;}
+
+    private Room _room;
+    public Room Room => _room;
+    public event Action OnDataChanged;
+    private void Awake()
+    {
+        Instance = this;
+    }
+    
+    // 방 입장에 성공하면 자동으로 호출되는 콜백 함수
+    public override void OnJoinedRoom()
+    {
+        // Debug.Log("룸 입장 완료!");
+        //
+        // Debug.Log($"룸: {PhotonNetwork.CurrentRoom.Name}");
+        // Debug.Log($"플레이어 인원: {PhotonNetwork.CurrentRoom.PlayerCount}");
+        //
+        // // 룸에 입장한 플레이어 정보
+        // Dictionary<int, Player> roomPlayers = PhotonNetwork.CurrentRoom.Players;
+        // foreach (KeyValuePair<int, Player> player in roomPlayers)
+        // {
+        //     Debug.Log($"{player.Value.NickName} : {player.Value.ActorNumber}");
+        // }
+        
+        _room = PhotonNetwork.CurrentRoom;
+        OnDataChanged?.Invoke();
+        
+        // 리소스 폴더에서 "Player" 이름을 가진 프리팹을 생성(인스턴스화)하고, 서버에 등록도 한다
+        // -> 리소스 폴더 대신 다른 방법 찾아보기
+        PhotonNetwork.Instantiate("Player", SpawnManager.Instance.GetRandomSpawnPosition(), Quaternion.identity);
+    }
+}
